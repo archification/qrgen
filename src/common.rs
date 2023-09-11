6abe2d116a11d8035e1c0f4ba9c28f2a9bbc1887
archification <archification@gmail.com>
2023-09-11 01:18:19 -0400

    file split

---
+++ src/common.rs
@@ -0,0 +1,56 @@
+use std::path::Path;
+use std::io::{self, Read};
+use crate::solarized::{
+    print_colored, print_fancy,
+    VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA,
+    BOLD, UNDERLINED, ITALIC
+};
+use qrcode_generator::QrCodeEcc;
+
+pub fn png(url: &str, filename: &str) {
+    print_colored(
+        &["c", "r", "e", "a", "t", "i", "n", "g"],
+        &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA]
+    );
+    qrcode_generator::to_png_to_file(url, QrCodeEcc::Low, 1024, filename).unwrap();
+}
+
+pub fn svg(url: &str, filename: &str) {
+    print_colored(
+        &["c", "r", "e", "a", "t", "i", "n", "g"],
+        &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA]
+    );
+    qrcode_generator::to_svg_to_file(url, QrCodeEcc::Low, 1024, None::<&str>, filename).unwrap();
+}
+
+pub fn read_file_to_string(filename: &str) -> io::Result<String> {
+    let mut file = std::fs::File::open(filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+    //2K cap
+}
+
+pub fn is_file_path(path: &str) -> bool {
+    Path::new(path).is_file()
+}
+
+pub fn usage(args: Vec<String>) {
+    print_fancy(&[
+        ("Usage: ", CYAN, vec![BOLD]),
+        (&format!("{}", args[0]), VIOLET, vec![]),
+        (" <text>", MAGENTA, vec![]),
+        (" <fileName>\n", GREEN, vec![]),
+        ("Example: ", CYAN, vec![BOLD]),
+        (&format!("{} ", args[0]), BLUE, vec![]),
+        ("\"", MAGENTA, vec![]),
+        ("https://google.com/", MAGENTA, vec![UNDERLINED, ITALIC]),
+        ("\"", MAGENTA, vec![]),
+        (" GoogleQrImage.png\n", GREEN, vec![]),
+        ("<text>", MAGENTA, vec![]),
+        (" is required. Put your url here in quotes.\n", CYAN, vec![]),
+        ("<fileName>", GREEN, vec![]),
+        (" is optional. Can be png or svg. Defaults to \"file_output.png\".\n", CYAN, vec![]),
+        ("Image file is generated in current directory.", CYAN, vec![]),
+    ]);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,19 @@
 mod solarized;
+mod common;
 
 use std::path::{Path, PathBuf};
 use std::io::{self, Read};
 use std::env;
 use solarized::{
-    print_colored, print_fancy,
-    VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA,
-    BOLD, UNDERLINED, ITALIC
+    print_fancy,
+    BLUE, CYAN, GREEN, RED,
+    BOLD,
 };
-use qrcode_generator::QrCodeEcc;
+use common::{
+    png, svg, read_file_to_string, is_file_path, usage
+};
 
 const MAX_TEXT_SIZE: usize = 2048;
-
-fn png(url: &str, filename: &str) {
-    print_colored(
-        &["c", "r", "e", "a", "t", "i", "n", "g"],
-        &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA]
-    );
-    qrcode_generator::to_png_to_file(url, QrCodeEcc::Low, 1024, filename).unwrap();
-}
-
-fn svg(url: &str, filename: &str) {
-    print_colored(
-        &["c", "r", "e", "a", "t", "i", "n", "g"],
-        &[VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA]
-    );
-    qrcode_generator::to_svg_to_file(url, QrCodeEcc::Low, 1024, None::<&str>, filename).unwrap();
-}
-
-fn read_file_to_string(filename: &str) -> io::Result<String> {
-    let mut file = std::fs::File::open(filename)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    Ok(contents)
-    //2K cap
-}
-
-fn is_file_path(path: &str) -> bool {
-    Path::new(path).is_file()
-}
-
-fn usage(args: Vec<String>) {
-    print_fancy(&[
-        ("Usage: ", CYAN, vec![BOLD]),
-        (&format!("{}", args[0]), VIOLET, vec![]),
-        (" <text>", MAGENTA, vec![]),
-        (" <fileName>\n", GREEN, vec![]),
-        ("Example: ", CYAN, vec![BOLD]),
-        (&format!("{} ", args[0]), BLUE, vec![]),
-        ("\"", MAGENTA, vec![]),
-        ("https://google.com/", MAGENTA, vec![UNDERLINED, ITALIC]),
-        ("\"", MAGENTA, vec![]),
-        (" GoogleQrImage.png\n", GREEN, vec![]),
-        ("<text>", MAGENTA, vec![]),
-        (" is required. Put your url here in quotes.\n", CYAN, vec![]),
-        ("<fileName>", GREEN, vec![]),
-        (" is optional. Can be png or svg. Defaults to \"file_output.png\".\n", CYAN, vec![]),
-        ("Image file is generated in current directory.", CYAN, vec![]),
-    ]);
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
