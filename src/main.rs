69d37fc86668181603e18e4e9f13a9296d1c090f
archification <archification@gmail.com>
2023-09-10 23:25:36 -0400

    add stdin support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod solarized;
 
+use std::path::{Path, PathBuf};
+use std::io::{self, Read};
+use std::env;
 use solarized::{
     print_colored, print_fancy,
     VIOLET, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED, MAGENTA,
     BOLD, UNDERLINED, ITALIC
 };
 use qrcode_generator::QrCodeEcc;
-use std::path::{Path, PathBuf};
-use std::env;
 
 fn png(url: &str, filename: &str) {
     print_colored(
@@ -25,40 +26,54 @@ fn svg(url: &str, filename: &str) {
     qrcode_generator::to_svg_to_file(url, QrCodeEcc::Low, 1024, None::<&str>, filename).unwrap();
 }
 
+fn usage(args: Vec<String>) {
+    print_fancy(&[
+        ("Usage: ", CYAN, vec![BOLD]),
+        (&format!("{}", args[0]), VIOLET, vec![]),
+        (" <text>", MAGENTA, vec![]),
+        (" <fileName>\n", GREEN, vec![]),
+        ("Example: ", CYAN, vec![BOLD]),
+        (&format!("{} ", args[0]), BLUE, vec![]),
+        ("\"", MAGENTA, vec![]),
+        ("https://google.com/", MAGENTA, vec![UNDERLINED, ITALIC]),
+        ("\"", MAGENTA, vec![]),
+        (" GoogleQrImage.png\n", GREEN, vec![]),
+        ("<text>", MAGENTA, vec![]),
+        (" is required. Put your url here in quotes.\n", CYAN, vec![]),
+        ("<fileName>", GREEN, vec![]),
+        (" is optional. Can be png or svg. Defaults to \"file_output.png\".\n", CYAN, vec![]),
+        ("Image file is generated in current directory.", CYAN, vec![]),
+    ]);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 && args.len() != 3 {
-        print_fancy(&[
-            ("Usage: ", CYAN, vec![BOLD]),
-            (&format!("{}", args[0]), VIOLET, vec![]),
-            (" <text>", MAGENTA, vec![]),
-            (" <fileName>\n", GREEN, vec![]),
-            ("Example: ", CYAN, vec![BOLD]),
-            (&format!("{} ", args[0]), BLUE, vec![]),
-            ("\"", MAGENTA, vec![]),
-            ("https://google.com/", MAGENTA, vec![UNDERLINED, ITALIC]),
-            ("\"", MAGENTA, vec![]),
-            (" GoogleQrImage.png\n", GREEN, vec![]),
-            ("<text>", MAGENTA, vec![]),
-            (" is required. Put your url here in quotes.\n", CYAN, vec![]),
-            ("<fileName>", GREEN, vec![]),
-            (" is optional. Can be png or svg. Defaults to \"file_output.png\".\n", CYAN, vec![]),
-            ("Image file is generated in current directory.", CYAN, vec![]),
-        ]);
+    let (url, filename) = if args.len() == 1 {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+        let parts: Vec<&str> = buffer.trim().split_whitespace().collect();
+        if parts.len() == 1 {
+            (parts[0].to_string(), "file_output.png".to_string())
+        } else if parts.len() >= 2 {
+            (parts[0].to_string(), parts[1].to_string())
+        } else {
+            usage(args);
+            return;
+        }
+    } else if args.len() == 2 {
+        (args[1].clone(), "file_output.png".to_string())
+    } else if args.len() == 3 {
+        (args[1].clone(), args[2].clone())
+    } else {
+        usage(args);
         return;
-    }
-    let url = &args[1];
-    let filename = match args.as_slice() {
-        [_] | [_ , _] => "file_output.png",
-        [_, _, filename, ..] => filename,
-        _ => unreachable!(),
     };
-    let extension = Path::new(filename).extension()
+    let extension = Path::new(&filename).extension()
         .and_then(|s| s.to_str())
         .unwrap_or("");
     match extension {
-        "png" => png(url, filename),
-        "svg" => svg(url, filename),
+        "png" => png(&url, &filename),
+        "svg" => svg(&url, &filename),
         _ => {
             let mut new_filename = PathBuf::from(filename);
             new_filename.set_extension("png");
@@ -71,7 +86,7 @@ fn main() {
                 (". Saving as ", CYAN, vec![]),
                 (new_filename_str, BLUE, vec![BOLD]),
             ]);
-            png(url, new_filename_str)
+            png(&url, new_filename_str)
         },
     }
 }
